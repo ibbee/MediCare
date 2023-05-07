f5c593b84be973b79d661f715cbb1ae7e019bab1
Hassan Mahmood <hassanarifmahmood@gmail.com>
2023-05-07 16:19:32 +0500

    Created RegisterActivity
    Intent also made in LoginActivity

--- app/src/main/java/com/practice/medicare/LoginActivity.java
+++ app/src/main/java/com/practice/medicare/LoginActivity.java
@@ -2,6 +2,7 @@ package com.practice.medicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
         btn = findViewById(R.id.buttonLogin);
         tv = findViewById(R.id.textViewNewUser);
 
+//        login button listener
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +39,14 @@ public class LoginActivity extends AppCompatActivity {
                 else {
                     Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+//        register text button
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class)); //current activity, jumping activity
             }
         });
     }
