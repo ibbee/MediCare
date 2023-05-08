2d539def7855843b3cfe4137fe7fdbd7dee275f6
Hafiz Uzair <hafiz666152001@gmail.com>
2023-05-08 17:38:25 +0500

    Health Article

---
+++ app/src/main/java/com/practice/medicare/HealthArticleActivity.java
@@ -0,0 +1,87 @@
+package com.practice.medicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class HealthArticleActivity extends AppCompatActivity {
+
+    private String[][] health_details =
+            {
+                    {"Walking Daily","","","","Click Here"},
+                    {"Home care of COVID-19","","","","Click More Details"},
+                    {"Stop Smoking","","","","Click More Details"},
+                    {"Menstrual Cramps","","","","Click More Details"},
+                    {"Healthy Gut","","","","Click More Details"}
+            };
+    private int[] images =
+            {
+                    R.drawable.health1,
+                    R.drawable.health2,
+                    R.drawable.health3,
+                    R.drawable.health4,
+                    R.drawable.health5
+            };
+
+    HashMap<String,String> item;
+    ArrayList list;
+    SimpleAdapter sa;
+    Button  btnBack;
+    ListView lst;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_article_activity);
+        lst=findViewById(R.id.listViewHA);
+        btnBack=findViewById(R.id.buttonHABack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HealthArticleActivity.this, HomeActivity.class));
+            }
+        });
+
+        list = new ArrayList();
+        for (int i=0;i<health_details.length;i++) {
+            item = new HashMap<String, String>();
+            item.put("line1", health_details[i][0]);
+            item.put("line2", health_details[i][1]);
+            item.put("line3", health_details[i][2]);
+            item.put("line4", health_details[i][3]);
+            item.put("line5", health_details[i][4]);
+            list.add(item);
+        }
+
+        sa = new SimpleAdapter(this, list, R.layout.multi_lines,
+                new String[]{"line1","line2","line3","line4","line5"}, //from
+                new int[]{R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e}); //to
+
+        lst.setAdapter(sa);
+
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it = new Intent(HealthArticleActivity.this, HealthArticlesDetailsActivity.class);
+                it.putExtra("text1", health_details[i][0]);
+                it.putExtra("text2", images[i]);
+                startActivity(it);
+            }
+        });
+
+
+
+        }
+}
--- app/src/main/java/com/practice/medicare/HomeActivity.java
+++ app/src/main/java/com/practice/medicare/HomeActivity.java
@@ -49,5 +49,13 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(HomeActivity.this, LabTestActivity.class));
             }
         });
+
+        CardView  health = findViewById(R.id.cardHealthDoctor);
+        health.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, HealthArticleActivity.class));
+            }
+        });
     }
 }
